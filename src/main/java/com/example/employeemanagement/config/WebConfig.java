174744f0dbb05984fe302aa9a917a101c414415c
Jorge Salas <jsalopdev@gmail.com>
2024-06-12 21:43:27 -0500

    cors_urls

--- src/main/java/com/example/employeemanagement/config/WebConfig.java
+++ src/main/java/com/example/employeemanagement/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:3000") // Añade aquí tu URL de Railway
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
