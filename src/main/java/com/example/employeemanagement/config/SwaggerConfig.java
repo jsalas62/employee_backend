362aae028f82e32a3460d6c48a7e2a4cce5f9652
Jorge Salas <jsalopdev@gmail.com>
2024-06-13 12:15:04 -0500

    swagger_production

--- src/main/java/com/example/employeemanagement/config/SwaggerConfig.java
+++ src/main/java/com/example/employeemanagement/config/SwaggerConfig.java
@@ -1,13 +1,13 @@
 package com.example.employeemanagement.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SwaggerConfig implements WebMvcConfigurer {
+public class SwaggerConfig {
 
     @Bean
     public GroupedOpenApi publicApi() {
@@ -17,11 +17,11 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .build();
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Employee Management API")
+                        .version("1.0")
+                        .description("API for Employee Management System"));
     }
 }
--- src/main/java/com/example/employeemanagement/config/WebConfig.java
+++ src/main/java/com/example/employeemanagement/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://employeefrontend-production.up.railway.app/", "https://calm-youth-production.up.railway.app/swagger-ui/index.html")
+                        .allowedOrigins("http://localhost:3000", "https://employeefrontend-production.up.railway.app", "https://calm-youth-production.up.railway.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
