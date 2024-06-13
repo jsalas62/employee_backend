477197328f170da913fa13b0ccf67e3add296661
Jorge Salas <jsalopdev@gmail.com>
2024-06-13 10:52:07 -0500

    swagger_prod

--- src/main/java/com/example/employeemanagement/config/WebConfig.java
+++ src/main/java/com/example/employeemanagement/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://employeefrontend-production.up.railway.app/")
+                        .allowedOrigins("http://localhost:3000", "https://employeefrontend-production.up.railway.app/", "https://calm-youth-production.up.railway.app/swagger-ui/index.html")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
