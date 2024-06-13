af7f47e656c6a7bab811bb5bdf48d89db69311d5
Jorge Salas <jsalopdev@gmail.com>
2024-06-13 09:37:13 -0500

    swagger_config

--- src/main/java/com/example/employeemanagement/config/SwaggerConfig.java
+++ src/main/java/com/example/employeemanagement/config/SwaggerConfig.java
@@ -1,9 +1,10 @@
 package com.example.employeemanagement.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springdoc.core.GroupedOpenApi;
 
 @Configuration
 public class SwaggerConfig implements WebMvcConfigurer {
@@ -11,14 +12,14 @@ public class SwaggerConfig implements WebMvcConfigurer {
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
-                .group("springshop-public")
+                .group("public")
                 .pathsToMatch("/api/**")
                 .build();
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui.html")
+        registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
